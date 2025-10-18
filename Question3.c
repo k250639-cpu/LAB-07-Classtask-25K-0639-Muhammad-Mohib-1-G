9d80715c69f7cb0470b0637ea90fed97c983c686
Muhammad Mohib <k250639@nu.edu.pk>
2025-10-18 08:53:40 -0700

    Update Question3.c

--- Question3.c
+++ Question3.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
-int main(){
+int main() {
     int pass[10] = {0};
     int fail[10] = {0};
-    int pcount=0, fcount=0, ptotal = 0, ftotal = 0, num;
-    for(int i = 0; i < 10; i++){
-        printf("\nEnter number of student no.%d (Enter -1 to exit): ", i + 1);
+    int pcount = 0, fcount = 0;
+    float ptotal = 0, ftotal = 0;
+    int num;
+    for (int i = 0; i < 10; i++) {
+        printf("Enter marks of student no.%d (Enter -1 to exit): ", i + 1);
         scanf("%d", &num);
-        if(num == -1)
+        if (num == -1)
             break;
-        if(num >= 5){
+        if (num >= 5 && num <= 10) {
             pass[pcount] = num;
-            pcount += 1;
             ptotal += num;
-        } else if(num >= 0 && num < 5){
+            pcount+=1;
+        } 
+        else if (num >= 0 && num < 5) {
             fail[fcount] = num;
-            fcount += 1;
             ftotal += num;
+            fcount+=1;
+        } 
+        else {
+            printf("Invalid marks! Please enter between 0 and 10.\n");
+            i--;
         }
     }
-    printf("Passing scores: ");
-    for(int i = 0; i < pcount; i++){
-        printf("%d, ", pass[i]);
+    printf("\nPassing scores: ");
+    if (pcount > 0) {
+        for (int i = 0; i < pcount; i++)
+            printf("%d ", pass[i]);
+        printf("\nAverage of passing scores: %.2f\n", ptotal / pcount);
+    } else {
+        printf("None\n");
     }
-    if(pcount > 0)
-        printf("\nThe average of the passing scores is: %d\n", ptotal / pcount);
-    else
-        printf("\nNo passing scores entered.\n");
     printf("Failing scores: ");
-    for(int i = 0; i < fcount; i++){
-        printf("%d, ", fail[i]);
+    if (fcount > 0) {
+        for (int i = 0; i < fcount; i++)
+            printf("%d ", fail[i]);
+        printf("\nAverage of failing scores: %.2f\n", ftotal / fcount);
+    } else {
+        printf("None\n");
     }
-    if(fcount > 0)
-        printf("\nThe average of the failing scores is: %d\n", ftotal / fcount);
-    else
-        printf("\nNo failing scores entered.\n");
-
     return 0;
 }
