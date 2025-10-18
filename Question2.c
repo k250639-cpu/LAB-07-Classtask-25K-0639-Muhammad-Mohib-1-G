c3ec5b08444358be446c2e5470b8989f8ec300c7
Muhammad Mohib <k250639@nu.edu.pk>
2025-10-18 08:48:35 -0700

    Update Question2.c

--- Question2.c
+++ Question2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int main(){
-	int arr[10] = {0};
-	int num=0, count = 0;
-	printf("Enter 10 numbers to be stored in an array");
-	for(int i =0; i < 10; i++){
-		scanf("%d", & arr[i]);
-	}
-	printf("Enter number to be searched in the array: ");
-	scanf("%d", & num);
-	for(int i = 0; i < 10; i++){
-		if( arr[i] == num){
-			count += 1;
-		}
-	}
-	if(count >=1){
-		printf("The number %d was seen %d times in the array", num, count);
-	}else{
-		printf("number not found");
-	}
-	return 0;
+int main() {
+    int arr[10];
+    int num, count = 0;
+    printf("Enter 10 numbers to be stored in an array:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter number to be searched in the array: ");
+    scanf("%d", &num);
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] == num) {
+            count+=1;
+        }
+    }
+    if (count >= 1) {
+        printf("The number %d was seen %d times in the array.\n", num, count);
+    } else {
+        printf("number not found\n");
+    }
+    return 0;
 }
